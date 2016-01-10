1fc0cdc8df3787a971b0f229e0ed9d6979600c98
motss <wes_ngrongsen@hotmail.com>
2016-01-10 17:46:32 +0800

    :tiger2: patched install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp gulpfile.babel.js package.json .gitignore -i ./
+cp -i gulpfile.babel.js ../../ && npm install --save-dev babel-core babel-preset-es2015 del gulp gulp-autoprefixer gulp-babel gulp-cssnano gulp-html-extract gulp-htmlmin gulp-imagemin gulp-minify-html gulp-rename gulp-replace gulp-size gulp-uglify loadsh run-sequence
